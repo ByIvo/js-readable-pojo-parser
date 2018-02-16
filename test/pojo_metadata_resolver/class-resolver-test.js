895f305e628b76c0d804bc67e41111e13c3e6adf
Ivo Batistela <irbatistela@gmail.com>
2018-02-16 01:28:08 -0200

    was need to create a real test to generate output to coveralls

--- test/pojo_metadata_resolver/class-resolver-test.js
+++ test/pojo_metadata_resolver/class-resolver-test.js
@@ -1,7 +1,15 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
 
+var classResolver = require('./../../src/pojo_metadata_resolver/class-resolver.js');
 
 describe('test', function () {
   it('another test', function () {
+    var metadata = {};
+    var canResolve = classResolver('public class ClassName {', metadata);
 
+    expect(metadata.class).to.equal('ClassName');
+    expect(canResolve).to.equal(true);
   });
 });
